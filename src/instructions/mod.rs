33ec11ab4c028e68b4170301c60f297a2386d115
mrb1nary <anubhab.techie@gmail.com>
2025-07-22 17:53:25 +0530

    Added update config instruction

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,21 +1,23 @@
 pub mod init_multisig;
+pub mod update_multisig;
 
 pub use init_multisig::*;
+pub use update_multisig::*;
 
 use pinocchio::program_error::ProgramError;
 
 pub enum MultisigInstructions {
-    InitMultisig = 0, // Johnny + Raunit 
-    //update expiry
-    //update threshold
-    //update members
-    UpdateMultisig = 1, // Glacier + SOLDADDY + Zubayr + Yunohu
-    CreateProposal = 2, // Nishant + Umang
-    Vote = 3, // Shrinath + Mohammed + shradesh
-    // will close if expiry achieved & votes < threshold || execute if votes >= threshold
-    CloseProposal = 4, // Nanasi + Mishal + Apaar + Ghazal 
+    InitMultisig = 0, // Johnny + Raunit
+                      //update expiry
+                      //update threshold
+                      //update members
+                      //UpdateMultisig = 1, // Glacier + SOLDADDY + Zubayr + Yunohu
+                      // CreateProposal = 2, // Nishant + Umang
+                      //Vote = 3, // Shrinath + Mohammed + shradesh
+                      // will close if expiry achieved & votes < threshold || execute if votes >= threshold
+                      // CloseProposal = 4, // Nanasi + Mishal + Apaar + Ghazal
 
-    //Santoshi CHAD own version
+                      //Santoshi CHAD own version
 }
 
 impl TryFrom<&u8> for MultisigInstructions {
@@ -24,10 +26,10 @@ impl TryFrom<&u8> for MultisigInstructions {
     fn try_from(value: &u8) -> Result<Self, Self::Error> {
         match *value {
             0 => Ok(MultisigInstructions::InitMultisig),
-            1 => Ok(MultisigInstructions::UpdateMultisig),
-            2 => Ok(MultisigInstructions::CreateProposal),
-            3 => Ok(MultisigInstructions::Vote),
-            4 => Ok(MultisigInstructions::CloseProposal),
+            //1 => Ok(MultisigInstructions::UpdateMultisig),
+            //2 => Ok(MultisigInstructions::CreateProposal),
+            //3 => Ok(MultisigInstructions::Vote),
+            //4 => Ok(MultisigInstructions::CloseProposal),
             _ => Err(ProgramError::InvalidInstructionData),
         }
     }
