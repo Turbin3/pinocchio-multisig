8887424a975e2b7b4cc8762397bae4642d534c0d
zubayr1 <write2zubayr@gmail.com>
2025-09-12 13:48:49 +0200

    implement execute tx

--- src/state/transaction.rs
+++ src/state/transaction.rs
@@ -1,11 +1,16 @@
+use core::mem::MaybeUninit;
 use pinocchio::{
     account_info::AccountInfo,
     program_error::ProgramError,
+    pubkey::Pubkey,
+    instruction::{Instruction, Seed, Signer, AccountMeta},
     ProgramResult,
+    cpi::{MAX_CPI_ACCOUNTS, slice_invoke_signed},
 };
 use bytemuck::{Pod, Zeroable};
 use crate::instructions::create_transaction::CreateTransactionIxData;
 use crate::helper::account_init::StateDefinition;
+use crate::state::multisig::MultisigState;
 
 #[repr(C)]
 #[derive(Clone, Copy, Debug, PartialEq, shank::ShankAccount, Pod, Zeroable)]
@@ -45,6 +50,107 @@ impl TransactionState {
         transaction_state.tx_buffer = ix_data.tx_buffer;
         transaction_state.buffer_size = ix_data.buffer_size;
         transaction_state.bump = bump;
+
+        Ok(())
+    }
+
+    /// deserialize fun
+    fn deserialize_instruction(&self) -> Result<(Pubkey, &[u8]), ProgramError> {
+        let buffer = &self.tx_buffer[..self.buffer_size as usize];
+
+        if buffer.len() < 32 {
+            return Err(ProgramError::InvalidInstructionData);
+        }
+
+        let program_id_bytes: [u8; 32] = buffer[0..32].try_into()
+            .map_err(|_| ProgramError::InvalidInstructionData)?;
+        let program_id = Pubkey::from(program_id_bytes);
+
+        let instruction_data = &buffer[32..];
+
+        Ok((program_id, instruction_data))
+    }
+
+    fn get_account_metas<'a>(cpi_accounts_slice: &'a [&AccountInfo]) -> Result<&'a [AccountMeta<'a>], ProgramError> {
+        const UNINIT_META: MaybeUninit<AccountMeta> = MaybeUninit::<AccountMeta>::uninit();
+
+        let mut metas: [MaybeUninit<AccountMeta>; MAX_CPI_ACCOUNTS] = [UNINIT_META; MAX_CPI_ACCOUNTS];
+
+        if cpi_accounts_slice.len() > MAX_CPI_ACCOUNTS {
+            return Err(ProgramError::InvalidArgument);
+        }
+
+        for i in 0..cpi_accounts_slice.len() {
+            unsafe {
+                metas
+                    .get_unchecked_mut(i)
+                    .write(AccountMeta::from(*cpi_accounts_slice.get_unchecked(i)));
+            }
+        }
+
+        let meta_slice: &[AccountMeta<'a>] = unsafe {
+            core::slice::from_raw_parts(metas.as_ptr() as _, cpi_accounts_slice.len())
+        };
+
+        Ok(meta_slice)
+    }
+
+    /// execute tx fun
+    pub fn execute(tx_type: u8, accounts: &[&AccountInfo]) -> ProgramResult {
+        let account_info_iter = &mut accounts.iter();
+
+        let multisig_acc: &AccountInfo = account_info_iter.next().ok_or(ProgramError::InvalidAccountData)?;
+        let transaction_acc: &AccountInfo = account_info_iter.next().ok_or(ProgramError::InvalidAccountData)?;
+
+        let cpi_accounts_slice: &[&AccountInfo] = account_info_iter.as_slice();
+
+        let multisig_state = MultisigState::from_account_info(multisig_acc)?;
+        let transaction_state = Self::from_account_info(transaction_acc)?;
+
+        let (cpi_program_id, cpi_data_slice) = transaction_state.deserialize_instruction()?;
+
+        let meta_slice = Self::get_account_metas(cpi_accounts_slice)?;
+
+        let cpi_instruction = Instruction {
+            program_id: &cpi_program_id,
+            accounts: meta_slice,
+            data: cpi_data_slice,
+        };
+
+        let binding = multisig_state.bump.to_le_bytes();
+        let primary_seed_bytes = multisig_state.primary_seed.to_le_bytes();
+        let signer_seeds = [
+            Seed::from(MultisigState::SEED.as_bytes()),
+            Seed::from(&primary_seed_bytes),
+            Seed::from(&binding),
+        ];
+
+        let signers = [Signer::from(&signer_seeds[..])];
+
+        match tx_type {
+            0 => { // Base transaction - execute CPI
+                slice_invoke_signed(&cpi_instruction, cpi_accounts_slice, &signers)?;
+            },
+            1 => { // AddMember
+                // TODO: Implement member addition logic
+                // add_member(cpi_accounts_slice[0])?;
+            },
+            2 => { // RemoveMember
+                // TODO: Implement member removal logic
+                // remove_member(cpi_accounts_slice[0])?;
+            },
+            3 => { // ChangeThreshold
+                // TODO: Implement threshold change logic
+                // change_threshold(cpi_accounts_slice[0], cpi_data_slice[0])?;
+            },
+            4 => { // ChangeSpendingLimit
+                // TODO: Implement spending limit change logic
+                // change_spending_limit(cpi_accounts_slice[0], cpi_data_slice[0])?;
+            },
+            _ => {
+                return Err(ProgramError::InvalidInstructionData);
+            }
+        }
 
         Ok(())
     }
